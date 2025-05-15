2a48fa90fc18d0d5970cb91afd16c1fa0c00d4c0
SoonWookHwang <108779266+SoonWookHwang@users.noreply.github.com>
2025-05-15 14:24:34 +0900

    first init

---
+++ src/main/java/com/lgcns/goows/GoowsApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns.goows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoowsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoowsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lgcns/goows/GoowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgcns.goows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoowsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
