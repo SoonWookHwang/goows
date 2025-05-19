6e680dee8eb05adb8ad72ba50027619ef3fd9de6
SoonWookHwang <108779266+SoonWookHwang@users.noreply.github.com>
2025-05-19 11:05:11 +0900

    FIX: .env 파일 제거, kafka

---
+++ src/main/java/com/lgcns/goows/global/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.lgcns.goows.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("JWT"))
+                .components(new Components().addSecuritySchemes("JWT", createAPIKeyScheme()))
+                .info(new Info().title("Goows API Docs")
+                        .description("뉴스 스크랩 및 머신러닝 기반 뉴스추천 어플리케이션 Goows의 Api 명세입니다")
+                        .version("v0.0.1"));
+    }
+}
